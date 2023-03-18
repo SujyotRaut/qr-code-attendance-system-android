41f6684b3415e4004d78966373353b69c28dfc9e
Sujyot <70472979+SujyotRaut@users.noreply.github.com>
2023-03-18 19:17:09 +0530

    Update MainActivity

--- app/src/main/java/com/example/qrcodeattendancesystem/MainActivity.kt
+++ app/src/main/java/com/example/qrcodeattendancesystem/MainActivity.kt
@@ -4,16 +4,20 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.qrcodeattendancesystem.activities.*
 import com.example.qrcodeattendancesystem.ui.theme.QRCodeAttendanceSystemTheme
 
@@ -36,22 +40,69 @@ fun MainScreen() {
         modifier = Modifier.fillMaxSize(),
         color = MaterialTheme.colorScheme.background) {
 
-        Column {
+        Column(modifier = Modifier.fillMaxSize()) {
 
-            Button(onClick = { context.startActivity(Intent(context, LoginActivity::class.java)) }) {
-                Text(text = "Log In")
+            Box(modifier = Modifier
+                .fillMaxWidth()
+                .aspectRatio(1.2f)
+                .background(MaterialTheme.colorScheme.primary)) {
+
+                Image(modifier = Modifier
+                    .width(200.dp)
+                    .align(Alignment.Center),
+                    painter = painterResource(id = R.drawable.default_profile_picture),
+                    contentDescription = "Profile Image")
             }
 
-            Button(onClick = { context.startActivity(Intent(context, QRCodeScannerActivity::class.java)) }) {
-                Text(text = "QR Code Scanner")
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+                .padding(16.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+
+                Column(modifier = Modifier.weight(1f)) {
+                    Text(text = "Student ID")
+                    Text(text = "Course")
+                    Text(text = "Year")
+                    Text(text = "Semester")
+                    Text(text = "Branch")
+                }
+
+                Column {
+                    Text(text = ":")
+                    Text(text = ":")
+                    Text(text = ":")
+                    Text(text = ":")
+                    Text(text = ":")
+                }
+
+                Column(modifier = Modifier.weight(1f)) {
+                    Text(text = "TBE19138")
+                    Text(text = "BE")
+                    Text(text = "4th")
+                    Text(text = "8th")
+                    Text(text = "CSE")
+                }
             }
 
-            Button(onClick = { context.startActivity(Intent(context, MarkAttendanceActivity::class.java)) }) {
-                Text(text = "Mark Attendance")
-            }
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)) {
 
-            Button(onClick = { context.startActivity(Intent(context, ViewAttendanceActivity::class.java)) }) {
-                Text(text = "View Attendance")
+                MyButton(modifier = Modifier.weight(1f),
+                    text = "Attendance",
+                    onClick = {
+                        val intent = Intent(context, ViewAttendanceActivity::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+                        context.startActivity(intent) })
+
+                MyButton(modifier = Modifier.weight(1f),
+                    text = "Scan QR",
+                    onClick = {
+                        val intent = Intent(context, QRCodeScannerActivity::class.java)
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY)
+                        context.startActivity(intent) })
             }
         }
     }
